bdb611e9bb000076576f9b007c5dfaa27674a418
Amir Yahalom <amir@blox.io>
2022-07-27 15:11:50 +0300

    ignore topic checks in msg validator

--- network/topics/msg_validator.go
+++ network/topics/msg_validator.go
@@ -1,10 +1,8 @@
 package topics
 
 import (
-	"bytes"
 	"context"
 	"github.com/bloxapp/ssv/network/forks"
-	"github.com/bloxapp/ssv/protocol/v1/message"
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"go.uber.org/zap"
@@ -19,7 +17,7 @@ type MsgValidatorFunc = func(ctx context.Context, p peer.ID, msg *pubsub.Message
 func NewSSVMsgValidator(plogger *zap.Logger, fork forks.Fork, self peer.ID) func(ctx context.Context, p peer.ID, msg *pubsub.Message) pubsub.ValidationResult {
 	return func(ctx context.Context, p peer.ID, pmsg *pubsub.Message) pubsub.ValidationResult {
 		topic := pmsg.GetTopic()
-		logger := plogger.With(zap.String("topic", topic), zap.String("peer", p.String()))
+		//logger := plogger.With(zap.String("topic", topic), zap.String("peer", p.String()))
 		metricsPubsubMsgValidationStart.WithLabelValues(topic).Inc()
 		defer metricsPubsubMsgValidationDone.WithLabelValues(topic).Inc()
 		//logger.Debug("validating msg")
@@ -28,42 +26,41 @@ func NewSSVMsgValidator(plogger *zap.Logger, fork forks.Fork, self peer.ID) func
 			reportValidationResult(validationResultNoData)
 			return pubsub.ValidationReject
 		}
-		if bytes.Equal([]byte(p), []byte(self)) {
+		if p == self {
 			reportValidationResult(validationResultSelf)
 			return pubsub.ValidationAccept
 		}
-		msg, err := fork.DecodeNetworkMsg(pmsg.GetData())
+		_, err := fork.DecodeNetworkMsg(pmsg.GetData())
 		if err != nil {
-			// can't decode message
-			//logger.Debug("invalid: can't decode message", zap.Error(err))
 			reportValidationResult(validationResultEncoding)
 			return pubsub.ValidationReject
 		}
 		// check decided topic
-		currentTopic := pmsg.GetTopic()
-		if msg.MsgType == message.SSVDecidedMsgType {
-			if decidedTopic := fork.DecidedTopic(); len(decidedTopic) > 0 {
-				if fork.GetTopicFullName(decidedTopic) == currentTopic {
-					reportValidationResult(validationResultValid)
-					return pubsub.ValidationAccept
-				}
-			}
-		}
-		topics := fork.ValidatorTopicID(msg.GetIdentifier().GetValidatorPK())
-		// check wrong topic
-		if fork.GetTopicFullName(topics[0]) != currentTopic {
-			// check second topic
-			// TODO: remove after forks
-			if len(topics) == 1 || fork.GetTopicFullName(topics[1]) != currentTopic {
-				logger.Debug("invalid: wrong topic",
-					zap.Strings("actual", topics),
-					zap.String("type", msg.MsgType.String()),
-					zap.String("expected", fork.GetTopicBaseName(currentTopic)),
-					zap.ByteString("smsg.ID", msg.GetIdentifier()))
-				reportValidationResult(validationResultTopic)
-				return pubsub.ValidationReject
-			}
-		}
+		//currentTopic := pmsg.GetTopic()
+		//currentTopicBaseName := fork.GetTopicBaseName(currentTopic)
+		//if msg.MsgType == message.SSVDecidedMsgType {
+		//	if decidedTopic := fork.DecidedTopic(); len(decidedTopic) > 0 {
+		//		if decidedTopic == currentTopicBaseName {
+		//			reportValidationResult(validationResultValid)
+		//			return pubsub.ValidationAccept
+		//		}
+		//	}
+		//}
+		//topics := fork.ValidatorTopicID(msg.GetIdentifier().GetValidatorPK())
+		//// check wrong topic
+		//if topics[0] != currentTopicBaseName {
+		//	// check second topic
+		//	// TODO: remove after forks
+		//	if len(topics) == 1 || topics[1] != currentTopicBaseName {
+		//		logger.Debug("invalid: wrong topic",
+		//			zap.Strings("actual", topics),
+		//			zap.String("type", msg.MsgType.String()),
+		//			zap.String("expected", currentTopicBaseName),
+		//			zap.ByteString("smsg.ID", msg.GetIdentifier()))
+		//		reportValidationResult(validationResultTopic)
+		//		return pubsub.ValidationReject
+		//	}
+		//}
 		reportValidationResult(validationResultValid)
 		return pubsub.ValidationAccept
 	}
